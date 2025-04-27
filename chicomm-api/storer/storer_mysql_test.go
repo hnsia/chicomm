e4f7b0c94abe6291280f90fb5290306e50a01a23
hnsia <44498178+hnsia@users.noreply.github.com>
2025-04-28 01:14:32 +0800

    test: add Get Product DAO test

--- chicomm-api/storer/storer_mysql_test.go
+++ chicomm-api/storer/storer_mysql_test.go
@@ -39,3 +39,37 @@ func TestMySQLStorer_CreateProduct(t *testing.T) {
 	err = mock.ExpectationsWereMet()
 	require.NoError(t, err)
 }
+
+func TestMySQLStorer_GetProduct(t *testing.T) {
+	mockDB, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	if err != nil {
+		t.Fatalf("Failed to create mock database: %v", err)
+	}
+	defer mockDB.Close()
+
+	db := sqlx.NewDb(mockDB, "sqlmock")
+	st := NewMySQLStorer(db)
+
+	p := &Product{
+		Name:         "test product",
+		Image:        "test.jpg",
+		Category:     "test category",
+		Description:  "test description",
+		Rating:       4,
+		NumReviews:   100,
+		Price:        100.0,
+		CountInStock: 100,
+	}
+
+	rows := sqlmock.NewRows([]string{"id", "name", "image", "category", "description", "rating", "num_reviews", "price", "count_in_stock", "created_at", "updated_at"}).
+		AddRow(1, p.Name, p.Image, p.Category, p.Description, p.Rating, p.NumReviews, p.Price, p.CountInStock, p.CreatedAt, p.UpdatedAt)
+
+	mock.ExpectQuery("SELECT * FROM products WHERE id=?").WithArgs(1).WillReturnRows(rows)
+
+	gp, err := st.GetProduct(context.Background(), 1)
+	require.NoError(t, err)
+	require.Equal(t, int64(1), gp.ID)
+
+	err = mock.ExpectationsWereMet()
+	require.NoError(t, err)
+}
