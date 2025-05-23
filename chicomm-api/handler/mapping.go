45be863a466cea2d494d222a1c0a2360a410ad7c
hnsia <44498178+hnsia@users.noreply.github.com>
2025-05-23 21:39:35 +0800

    refactor: utility mapping functions and types

---
+++ chicomm-api/handler/mapping.go
@@ -0,0 +1,96 @@
+package handler
+
+import "github.com/hnsia/chicomm/chicomm-grpc/pb"
+
+func toPBProductReq(p ProductReq) *pb.ProductReq {
+	return &pb.ProductReq{
+		Id:           p.ID,
+		Name:         p.Name,
+		Image:        p.Image,
+		Category:     p.Category,
+		Description:  p.Description,
+		Rating:       p.Rating,
+		NumReviews:   p.NumReviews,
+		Price:        p.Price,
+		CountInStock: p.CountInStock,
+	}
+}
+
+func toProductRes(p *pb.ProductRes) ProductRes {
+	return ProductRes{
+		Name:         p.Name,
+		Image:        p.Image,
+		Category:     p.Category,
+		Description:  p.Description,
+		Rating:       p.Rating,
+		NumReviews:   p.NumReviews,
+		Price:        p.Price,
+		CountInStock: p.CountInStock,
+	}
+}
+
+func toPBOrderReq(o OrderReq) *pb.OrderReq {
+	return &pb.OrderReq{
+		PaymentMethod: o.PaymentMethod,
+		TaxPrice:      o.TaxPrice,
+		ShippingPrice: o.ShippingPrice,
+		TotalPrice:    o.TotalPrice,
+		Items:         toPBOrderItems(o.Items),
+	}
+}
+
+func toPBOrderItems(items []*OrderItem) []*pb.OrderItem {
+	var res []*pb.OrderItem
+	for _, item := range items {
+		res = append(res, &pb.OrderItem{
+			Name:      item.Name,
+			Quantity:  item.Quantity,
+			Image:     item.Image,
+			Price:     item.Price,
+			ProductId: item.ProductID,
+		})
+	}
+	return res
+}
+
+func toOrderRes(o *pb.OrderRes) OrderRes {
+	return OrderRes{
+		ID:            o.Id,
+		PaymentMethod: o.PaymentMethod,
+		TaxPrice:      o.TaxPrice,
+		ShippingPrice: o.ShippingPrice,
+		TotalPrice:    o.TotalPrice,
+		Items:         toOrderItems(o.Items),
+	}
+}
+
+func toOrderItems(items []*pb.OrderItem) []*OrderItem {
+	var res []*OrderItem
+	for _, item := range items {
+		res = append(res, &OrderItem{
+			Name:      item.Name,
+			Quantity:  item.Quantity,
+			Image:     item.Image,
+			Price:     item.Price,
+			ProductID: item.ProductId,
+		})
+	}
+	return res
+}
+
+func toPBUserReq(u UserReq) *pb.UserReq {
+	return &pb.UserReq{
+		Name:     u.Name,
+		Email:    u.Email,
+		Password: u.Password,
+		IsAdmin:  u.IsAdmin,
+	}
+}
+
+func toUserRes(u *pb.UserRes) UserRes {
+	return UserRes{
+		Name:    u.Name,
+		Email:   u.Email,
+		IsAdmin: u.IsAdmin,
+	}
+}
