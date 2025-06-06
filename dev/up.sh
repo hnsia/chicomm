749fa45aa2ec982bb21862c59dba78b377e64807
hnsia <44498178+hnsia@users.noreply.github.com>
2025-06-06 20:13:28 +0800

    chore: setup bash scripts

---
+++ dev/up.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# build containers and dockerfile
+dev/build.sh
+
+# bring up mysql container
+
+# wait for mysql container to come up
+
+# apply migration files
+
+# bring up rest of the services
